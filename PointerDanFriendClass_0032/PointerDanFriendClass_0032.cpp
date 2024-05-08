70e0e5b394361e01d372ddc6fff2bda327e3e708
Othallia <vianiputriocta6@gmail.com>
2024-05-08 11:47:43 +0700

    menambahkan int main

--- PointerDanFriendClass_0032/PointerDanFriendClass_0032.cpp
+++ PointerDanFriendClass_0032/PointerDanFriendClass_0032.cpp
@@ -9,6 +9,14 @@ public:
     }
 };
 
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
 int main()
 {
     mahasiswa mhs{ 1 };
